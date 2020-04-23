348acdde8022163d1e30d288cb6c16a4b580c594
Ciabas <ciabas12@gmail.com>
2020-04-23 21:07:01 +0200

    [add] roll

---
+++ src/losowanie.js
@@ -0,0 +1,9 @@
+export default function(msg){
+  if (msg.content.match(/^!losowanie/)) {
+
+    // const number = Number(msg.content.match(/\d+/)[0]) + 1;
+    // msg.reply(number + ' wygrałem!')
+    // msg.channel.send(msg.author)
+  }
+}
+a
