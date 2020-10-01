1c2fd50af3e818241847f9f4b9636c18fd169258
Ciabas <a.wroblewski@binarapps.com>
2020-10-01 20:38:44 +0200

    [add] cmd bociosz

--- src/bociosz.js
+++ src/bociosz.js
@@ -4,10 +4,10 @@ export default function (msg) {
     msg.channel.send(`
     Oto dostępne komendy w ramach bociosza:
 
-    !clear x - pozwala uprawnionym osobistościom czyścić x ostatnich wiadomości, domyślnie x=10,
-    !roll x - rzut kością, domyślnie x=6,
-    !losowanie "adam, ewa, wonsz" - losuje z listy jedną wartość,
-    !losowanie "ciastko, kawa" "adam, ewa" - dopasowuje elementy list do siebie,
+    • !clear x - pozwala uprawnionym osobistościom czyścić x ostatnich wiadomości, domyślnie x=10,
+    • !roll x - rzut kością, domyślnie x=6,
+    • !losowanie "adam, ewa, wonsz" - losuje z listy jedną wartość,
+    • !losowanie "ciastko, kawa" "adam, ewa" - dopasowuje elementy list do siebie,
     `)
   }
 }
