2843291470a607406c2410e12ce02cbc0fdd6b14
Ciabas <a.wroblewski@binarapps.com>
2020-10-01 19:31:36 +0200

    mongo wip

---
+++ src/dataStorage.js
@@ -0,0 +1,15 @@
+'use strict';
+import fs from 'fs-sync'
+import { MongoClient } from 'mongodb'
+
+export const saveData = () => {
+
+}
+
+export const loadData = () => {
+  fs.readJSON('data.json', (err, data) => {
+    if (err) throw err;
+    let parsedData = JSON.parse(data);
+    console.log(data);
+  });
+}
