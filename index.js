62e42ac7500855dbc886c873b38030fec9935d65
Ciabas <a.wroblewski@binarapps.com>
2020-10-02 21:15:22 +0200

    [add] music

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import clear from './src/clear';
 import roll from './src/roll';
 import losowanie from './src/losowanie';
 import bociosz from './src/bociosz';
+import music from './src/music';
 
 var Discord = require('discord.js');
 var bot = new Discord.Client()
@@ -12,13 +13,22 @@ var bot = new Discord.Client()
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
+bot.once('reconnecting', () => {
+  console.log('Reconnecting!');
+});
+bot.once('disconnect', () => {
+  console.log('Disconnect!');
+});
 
 bot.on('message', function(msg) {
+  if (!message.content.startsWith('!')) return;
+  if (message.author.bot) return;
   picipolo(msg)
   clear(msg)
   roll(msg)
   losowanie(msg)
   bociosz(msg)
+  music(msg)
 })
 
 bot.login(process.env.DISCORD_TOKEN);
