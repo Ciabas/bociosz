6aca47115eacf4058d2631f0124874f96f92850a
Ciabas <a.wroblewski@binarapps.com>
2020-10-03 15:06:50 +0200

    [add] !play x to !bociosz

--- src/bociosz.js
+++ src/bociosz.js
@@ -5,10 +5,11 @@ export default function (msg) {
     msg.channel.send(`
     Oto dostępne komendy w ramach bociosza:
 
-    • ${prefix}clear x - pozwala uprawnionym osobistościom czyścić x ostatnich wiadomości, domyślnie x=10,
+    • ${prefix}clear x - pozwala uprawnionym osobistościom czyścić "x" ostatnich wiadomości, domyślnie x=10,
     • ${prefix}roll x - rzut kością, domyślnie x=6,
     • ${prefix}losowanie "adam, ewa, wonsz" - losuje z listy jedną wartość,
     • ${prefix}losowanie "ciastko, kawa" "adam, ewa" - dopasowuje elementy list do siebie,
+    • ${prefix}play x - bociosz wbija na kanał głosowy i gra, "x" to ID video z youtube,
     `)
   }
 }
