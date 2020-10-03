d8ab65993225e67a84fee760f614159a90cd48ab
Ciabas <a.wroblewski@binarapps.com>
2020-10-03 15:04:04 +0200

    [add] limit 20 to clear

--- src/clear.js
+++ src/clear.js
@@ -3,8 +3,10 @@ import { prefix } from './common';
 export default function(msg){
   if (msg.content.startsWith(`${prefix}clear`)){
     if(msg.member.hasPermission("MANAGE_MESSAGES")){
-      const count = msg.content.match(/\d+/) ? msg.content.match(/\d+/)[0] : 10
+      let count = msg.content.match(/\d+/) ? msg.content.match(/\d+/)[0] : 10
+      count = count > 20 ? 20 : count
       msg.channel.bulkDelete(count)
+      msg.channel.send(`Wywaliłem ${count} wiadomości`)
     }
     else {
       msg.channel.send("Nie masz praw cwaniaczku!")
