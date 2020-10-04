150411734cf4dbae45da9e1721a5178347a4f159
Ciabas <a.wroblewski@binarapps.com>
2020-10-04 18:15:30 +0200

    [add] music search by name

--- src/music.js
+++ src/music.js
@@ -1,22 +1,24 @@
-import ytdl  from 'ytdl-core';
+import ytdl from 'ytdl-core';
 import { prefix } from './common';
+import ytsr from 'ytsr';
 
 export default function (message){
-  const queue = createQueue(message);
+  const controlPanel = createControlPanel(message);
 
   if (message.content.startsWith(`${prefix}play`)) {
-    execute(queue);
+    setConnection(controlPanel);
+    addSong(controlPanel)
   }
   if (message.content.startsWith(`${prefix}skip`)) {
-    skip(queue);
+    skip(controlPanel);
   }
   if (message.content.startsWith(`${prefix}stop`)) {
-    stop(queue);
+    stop(controlPanel);
   }
 }
 
-const createQueue = (message) =>{
-  const createQueueConstruct = () => ({
+const createControlPanel = (message) =>{
+  const createControlPanelConstruct = () => ({
     textChannel: message.channel,
     voiceChannel: null,
     connection: null,
@@ -25,108 +27,118 @@ const createQueue = (message) =>{
     playing: true
   });
 
-  let queueContruct = createQueueConstruct()
+  let controlPanelContruct = createControlPanelConstruct()
 
   return {
     getMessage: () => message,
-    getVolume: () => queueContruct.volume,
-    getConnection: () => queueContruct.connection,
-    setConnection: (connection) => queueContruct.connection = connection,
-    setVoiceChannel: (voiceChannel) => queueContruct.voiceChannel = voiceChannel,
-    leaveVoiceChannel: () => queueContruct.voiceChannel.leave(),
-    resetState: () => queueContruct = createQueueConstruct(),
-    addSong: (song) => queueContruct.songs.push(song),
-    removeSong: (song) => queueContruct.songs.push(song),
-    getSong: (index = 0) => queueContruct.songs[index],
-    endDispatcher: () => queueContruct.connection.dispatcher.end(),
-    resetSongs: () => queueContruct.songs = [],
-    shiftFirstSong: () => queueContruct.songs.shift(),
-    sendMessage: (msg) => queueContruct.textChannel.send(msg),
+    getVolume: () => controlPanelContruct.volume,
+    getConnection: () => controlPanelContruct.connection,
+    setConnection: (connection) => controlPanelContruct.connection = connection,
+    setVoiceChannel: (voiceChannel) => controlPanelContruct.voiceChannel = voiceChannel,
+    leaveVoiceChannel: () => controlPanelContruct.voiceChannel.leave(),
+    resetState: () => controlPanelContruct = createControlPanelConstruct(),
+    addSong: (song) => controlPanelContruct.songs.push(song),
+    removeSong: (song) => controlPanelContruct.songs.push(song),
+    getSong: (index = 0) => controlPanelContruct.songs[index],
+    endDispatcher: () => controlPanelContruct.connection.dispatcher.end(),
+    resetSongs: () => controlPanelContruct.songs = [],
+    shiftFirstSong: () => controlPanelContruct.songs.shift(),
+    sendMessage: (msg) => controlPanelContruct.textChannel.send(msg),
     isConnectedToVoiceChannel: () => message.member.voice.channel,
   }
 }
 
-async function execute(queue) {
-  const message = queue.getMessage()
-  const args = message.content.split(" ");
+async function setConnection(controlPanel) {
+  const message = controlPanel.getMessage()
 
   const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel)
-    return queue.sendMessage(
+  if (!voiceChannel){
+    return controlPanel.sendMessage(
       "You need to be in a voice channel to play music!"
-    );
+      );
+  }
   const permissions = voiceChannel.permissionsFor(message.client.user);
   if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-    return queue.sendMessage(
+    return controlPanel.sendMessage(
       "I need the permissions to join and speak in your voice channel!"
-    );
+      );
   }
 
-  const songInfo = await ytdl.getInfo(args[1]);
+  if (!controlPanel.getConnection()) {
+    controlPanel.setVoiceChannel(voiceChannel)
+
+    try {
+      const connection = await voiceChannel.join();
+      controlPanel.setConnection(connection);
+    } catch (err) {
+      console.log(err);
+      controlPanel.leaveVoiceChannel()
+      controlPanel.resetState();
+      if(err){
+        controlPanel.sendMessage(`Error: ${err}`);
+      }
+    }
+  } else {
+    return controlPanel.sendMessage("No connection, try again later.");
+  }
+}
+
+function skip(controlPanel) {
+  if (!controlPanel.isConnectedToVoiceChannel())
+    return controlPanel.sendMessage(
+      "You have to be in a voice channel to stop the music!"
+    );
+  if (controlPanel.getConnection()) {
+    return controlPanel.endDispatcher()
+  }
+  return controlPanel.sendMessage("There is no song that I could skip!");
+}
+
+function stop(controlPanel) {
+  if (!controlPanel.isConnectedToVoiceChannel()) {
+    return controlPanel.sendMessage(
+      "You have to be in a voice channel to stop the music!"
+    );
+  }
+  controlPanel.resetSongs();
+  if (controlPanel.getConnection()) {
+    controlPanel.endDispatcher();
+  }
+}
+
+async function addSong(controlPanel){
+  const message = controlPanel.getMessage()
+  const songName = message.content.split(" ").slice(1).join(" ");
+
+  const songId = await ytsr(songName).then(r => {
+    const { link } = r.items[0]
+    return ytdl.getVideoID(link)
+  }).catch (err => controlPanel.sendMessage(`Can not find the song, ${err}`))
+
+  const songInfo = await ytdl.getInfo(songId);
   const song = {
     title: songInfo.videoDetails.title,
     url: songInfo.videoDetails.video_url
   };
 
-  queue.addSong(song)
-
-  if (!queue.getConnection()) {
-    queue.setVoiceChannel(voiceChannel)
-
-    try {
-      const connection = await voiceChannel.join();
-      queue.setConnection(connection);
-      play(queue.getSong(), queue);
-    } catch (err) {
-      console.log(err);
-      queue.leaveVoiceChannel()
-      queue.resetState();
-      if(err){
-        queue.sendMessage(`Error: ${err}`);
-      }
-    }
-  } else {
-    return queue.sendMessage(`${song.title} has been added to the queue!`);
-  }
+  controlPanel.addSong(song)
+  playSong(controlPanel.getSong(), controlPanel);
 }
 
-function skip(queue) {
-  if (!queue.isConnectedToVoiceChannel())
-    return queue.sendMessage(
-      "You have to be in a voice channel to stop the music!"
-    );
-  if (queue.getConnection()) {
-    return queue.endDispatcher()
-  }
-  return queue.sendMessage("There is no song that I could skip!");
-}
-
-function stop(queue) {
-  if (!queue.isConnectedToVoiceChannel()) {
-    return queue.sendMessage(
-      "You have to be in a voice channel to stop the music!"
-    );
-  }
-  queue.resetSongs();
-  if (queue.getConnection()) {
-    queue.endDispatcher();
-  }
-}
-
-function play(song, queue) {
+function playSong(song, controlPanel) {
   if (!song) {
-    queue.leaveVoiceChannel();
-    queue.resetState();
+    controlPanel.leaveVoiceChannel();
+    controlPanel.resetState();
     return;
   }
 
-  const dispatcher = queue.getConnection()
+  const dispatcher = controlPanel.getConnection()
     .play(ytdl(song.url, { quality: 'highestaudio', liveBuffer: 2000 }))
     .on("finish", () => {
-      queue.shiftFirstSong();
-      play(queue.getSong(), queue);
+      controlPanel.shiftFirstSong();
+      play(controlPanel.getSong(), controlPanel);
     })
     .on("error", error => console.error(error));
-  dispatcher.setVolumeLogarithmic(queue.getVolume() / 5);
-  queue.sendMessage(`Start playing: **${song.title}**`);
+  dispatcher.setVolumeLogarithmic(controlPanel.getVolume() / 5);
+  controlPanel.sendMessage(`Start playing: **${song.title}**`);
 }
