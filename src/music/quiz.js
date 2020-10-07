c0a7b91264c53a3a5d61466ff358eb06683f9304
Ciabas <a.wroblewski@binarapps.com>
2020-10-07 19:58:31 +0200

    [fix] quiz

--- src/music/quiz.js
+++ src/music/quiz.js
@@ -2,7 +2,7 @@ import quizSongs from './quizSongs.json'
 import { shuffle } from 'lodash'
 import play from './play'
 
-export function quiz(controlPanel){
+export default function quiz(controlPanel){
   const songs = shuffle(quizSongs).slice(0, 10)
   songs.map((song) => controlPanel.addSong(song))
   play(controlPanel)
