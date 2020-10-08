dbea7c4e2b65ca8233f035fef83ca428ac7ee269
Ciabas <a.wroblewski@binarapps.com>
2020-10-08 10:07:41 +0200

    [add] 429 yt catch error

--- src/music/play.js
+++ src/music/play.js
@@ -22,7 +22,12 @@ export default async function searchAndPlaySong(songName, controlPanel) {
 
     controlPanel.addSong(song)
     return playSong(controlPanel.shiftFirstSong(), controlPanel);
-  }).catch (err => controlPanel.sendMessage(`Can not get song's url, ${err}`))
+  }).catch (err => {
+    if(err.message === 'Status code: 429') {
+      controlPanel.sendMessage(`Limit of YT request reached, ${err}`)
+    }
+    controlPanel.sendMessage(`Can not get song's url, ${err}`)
+  })
 }
 
 function playSong(song, controlPanel) {
